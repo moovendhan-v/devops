08498398bdb87c39390987eebf4e5dc6e40aa2f2
agricreations <moovendhanagriculture@gmail.com>
2023-12-30 14:08:24 +0530

    home lab compose file

---
+++ portfolio/data/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# touch /var/photogram/hellowdocker
+git clone https://github.com/agricreation/moovendhanv.git /var/www/html
+# sed -i "s/short_open_tag = .*/short_open_tag = On/" /etc/php/8.1/apache2/php.ini
+/usr/sbin/apache2ctl -D FOREGROUND
